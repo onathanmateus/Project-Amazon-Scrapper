eb03afeb90e8bacfd6bea68a9597cc297074a401
Nathan Mateus <nathanmateudeo@hotmail.com>
2024-02-26 08:32:37 -0300

    feat: finish the project

--- index.js
+++ index.js
@@ -6,16 +6,27 @@ buttonElement.addEventListener("click", async () => {
   // Guardar na variável qual palavra foi digitada | Save which word was typed in the variable
   const keyword = keywordElement.value
   // Requisição à API | API request
-  const response = await fetch(`/api/scrape?keyword=${keyword}`)
+  const response = await fetch(`http://localhost:3333/api/scrape?keyword=${keyword}`)
   const products = await response.json()
   // Limpando da tela o que foi mostrado anteriormente | Wiping what was previously shown from the screen
   resultsElement.innerHTML = ""
+  const producstList = []
   // Exibindo os resultados na tela | Displaying the results on the screen
-  resultsElement.innerHTML = products.map(product => `
-    <div class="product">
-      <h1 class="product-title">${product.title}</h1>
-      <img src="${product.image}" />
-      <p class="product-information">Avaliação: ${product.rating} estrelas (${product.reviews})</p>
-    </div>
-  `).join('');
+  products.forEach(product => {
+    producstList.push( `
+      <div class="product-card">
+        <div class="image-container">
+          <img src="${product.image}" alt="Imagem do produto" />
+        </div>
+        <h3>${product.title}</h3>
+        <p>Avaliação: 🌟 ${product.rating}</p>
+        <p>Número de avaliações: ${product.reviews}</p>
+      </div>
+    `)
+  });
+
+  list.forEach(product => {
+    const item = document.createElement(product);
+    resultsElement.appendChild(item);
+  });
 }) 
---
+++ mock.js
@@ -0,0 +1,368 @@
+export const mock = 
+{
+  "metadata": {
+    "link": "https://www.amazon.com.br/s?k=celular"
+  },
+  "formattedProducts": [
+    {
+      "title": "Apple iPhone 15 (128 GB) — Verde",
+      "rating": "485",
+      "reviews": "125",
+      "image": "https://m.media-amazon.com/images/I/41BjCflNQDL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Apple iPhone 15 Pro Max (256 GB) — Titânio branco",
+      "rating": "475",
+      "reviews": "174",
+      "image": "https://m.media-amazon.com/images/I/81FFCoft1PL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Apple iPhone 11 (64 GB) (PRODUCT) RED",
+      "rating": "495",
+      "reviews": "2823",
+      "image": "https://m.media-amazon.com/images/I/715HCLsOHbL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Apple iPhone 14 (512 GB) – Amarelo",
+      "rating": "485",
+      "reviews": "4161",
+      "image": "https://m.media-amazon.com/images/I/51biUY32eEL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Motorola Moto E13 4G 64GB 4GB RAM Grafite",
+      "rating": "465",
+      "reviews": "1479",
+      "image": "https://m.media-amazon.com/images/I/51h9JwH1T3L._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Moto G14 128GB 4GB RAM Grafite",
+      "rating": "465",
+      "reviews": "737",
+      "image": "https://m.media-amazon.com/images/I/51aO+pBIwAL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Redmi 12 4G 256GB - 8GB Ram (Versao Global) (Midnight Black)",
+      "rating": "465",
+      "reviews": "1481",
+      "image": "https://m.media-amazon.com/images/I/61t99wqLeaL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Samsung Galaxy A14 128GB 5G Wi-Fi Tela 6.6'' Dual Chip 4GB RAM Câmera Tripla + Selfie 13MP Bateria de 5000mAh - Preto",
+      "rating": "475",
+      "reviews": "1116",
+      "image": "https://m.media-amazon.com/images/I/81nG28S7sHL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Redmi Note 13 8+256G Global Version Powerful Snapdragon® performance 120Hz FHD+ AMOLED display 33W fast charging with 5000mAh battery (Blue)",
+      "rating": "485",
+      "reviews": "67",
+      "image": "https://m.media-amazon.com/images/I/51TEjgf8bxL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Motorola Moto G53 5G 128GB 4GB RAM Prata",
+      "rating": "465",
+      "reviews": "2785",
+      "image": "https://m.media-amazon.com/images/I/51WS97L4FUL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Samsung A03 Core 32GB 2GB",
+      "rating": "465",
+      "reviews": "199",
+      "image": "https://m.media-amazon.com/images/I/41Pt-Bvs2bL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Nokia C2 2nd Edition 4G 32 GB Tela 5,7\" Câmera com IA Android Desbloqueio Facial + Capa/Película/Fone/Carregador - Cinza - NK085",
+      "rating": "405",
+      "reviews": "95",
+      "image": "https://m.media-amazon.com/images/I/61KO-Mw+xqL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Moto G54 5G 256GB 8GB RAM Azul - Vegan Leather",
+      "rating": "465",
+      "reviews": "207",
+      "image": "https://m.media-amazon.com/images/I/61jIQOl3vKL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Celular Xiaomi Redmi Note 12 128GB / 6GB RAM/Dual Sim/TelaP e 13MP - Onyx Gray - Preto",
+      "rating": "485",
+      "reviews": "17388",
+      "image": "https://m.media-amazon.com/images/I/61FQ61igtVL._AC_UL320_.jpg"
+    },
+    {
+      "title": "POCO C65 8GB+256GB NFC MediaTek Helio G85 Octa Core 5000mAh 6.74\" 90Hz HD+ display 50MP Camera Global Version (Purple)",
+      "rating": "465",
+      "reviews": "97",
+      "image": "https://m.media-amazon.com/images/I/514hAJCEjRL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Redmi 12C 128GB - 4GB Ram Ocean Blue (AZUL) (Azul)",
+      "rating": "475",
+      "reviews": "7317",
+      "image": "https://m.media-amazon.com/images/I/41XUT45dXlL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Samsung Galaxy A34 128GB 5G Tela 6.6'' Dual Chip 6GB RAM Câmera Tripla de até 48MP + Selfie 13MP Bateria de 5000mAh - Prata",
+      "rating": "475",
+      "reviews": "289",
+      "image": "https://m.media-amazon.com/images/I/61NnHQ7oDxL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Moto G84 5G 256GB 8GB RAM Grafite",
+      "rating": "465",
+      "reviews": "367",
+      "image": "https://m.media-amazon.com/images/I/51vjQLRYFzL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Redmi 12 4G 256GB - 8GB Ram (Versao Global) (Sky Blue)",
+      "rating": "465",
+      "reviews": "812",
+      "image": "https://m.media-amazon.com/images/I/41i2bARv0ZL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Samsung Galaxy A14 128GB 4G Wi-Fi Tela 6.6'' Dual Chip 4GB RAM Câmera Tripla de até 50MP + Selfie 13MP Bateria de 5000mAh - Preto",
+      "rating": "475",
+      "reviews": "1499",
+      "image": "https://m.media-amazon.com/images/I/61hCPNWZXyL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Apple iPhone 13 (512 GB) - Rosa",
+      "rating": "485",
+      "reviews": "18309",
+      "image": "https://m.media-amazon.com/images/I/41l+tImzWTL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Apple iPhone 12 (256 GB) - Azul",
+      "rating": "485",
+      "reviews": "7550",
+      "image": "https://m.media-amazon.com/images/I/51misXwIxAL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Apple iPhone 13 Pro Max (128 GB) - Grafite",
+      "rating": "485",
+      "reviews": "2247",
+      "image": "https://m.media-amazon.com/images/I/41vm9e97XZL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Apple iPhone 13 mini (128 GB) - Rosa",
+      "rating": "485",
+      "reviews": "1519",
+      "image": "https://m.media-amazon.com/images/I/41vmAS61syL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Moto G54 5G 128GB 4GB RAM Grafite",
+      "rating": "475",
+      "reviews": "138",
+      "image": "https://m.media-amazon.com/images/I/51HCLuhhdFL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Redmi 12 4G 256GB - 8GB Ram (Versao Global) (Polar Silver)",
+      "rating": "465",
+      "reviews": "489",
+      "image": "https://m.media-amazon.com/images/I/41hUnMEimkL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Redmi Note 13 Pro 5G 8GB+256GB Snapdragon 7s Gen 2, Câmera 200MP OIS, Carregamento Turbo 67W, 120Hz, AMOLED, NFC… (Black)",
+      "rating": "455",
+      "reviews": "34",
+      "image": "https://m.media-amazon.com/images/I/51pk5PzpzZL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Redmi Note 8 4GB Ram Tela 6.3 64GB Camera Quad 48+8+2+2MP - Branco",
+      "rating": "485",
+      "reviews": "4691",
+      "image": "https://m.media-amazon.com/images/I/51RIqTiRc8L._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Motorola Edge 40 Neo 256GB 8GB RAM Black Beauty",
+      "rating": "465",
+      "reviews": "94",
+      "image": "https://m.media-amazon.com/images/I/51rB2U+NQIL._AC_UL320_.jpg"
+    },
+    {
+      "title": "SmartSmartphone realme C55 RMX3710 Dual SIM 256GB / 8GB RAM de 6.72\" 64 + 2MP / 8MP - Sunshower",
+      "rating": "485",
+      "reviews": "501",
+      "image": "https://m.media-amazon.com/images/I/41kVj6BzyzL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Samsung Galaxy A03 Core 32GB 4G Wi-Fi Tela 6.5'' Dual Chip 2GB RAM Câmera 8MP + Selfie 5MP Bateria de 5000mAh - Cobre",
+      "rating": "435",
+      "reviews": "98",
+      "image": "https://m.media-amazon.com/images/I/81Usrxi6IiL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Redmi Note 12 Pro 5G Dual SIM 256GB - 8GB Ram (Versao Global) (Midnight black)",
+      "rating": "475",
+      "reviews": "5447",
+      "image": "https://m.media-amazon.com/images/I/31ielR1rgRL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Motorola Moto E13 4G 64GB 4GB RAM Off White",
+      "rating": "475",
+      "reviews": "305",
+      "image": "https://m.media-amazon.com/images/I/513fGxOVBpL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Celular Xiaomi Redmi 12C 3GB de RAM / 64GB / Tela 6.71''/ Dual Sim LTE - Graphite Cinza(2212ARNC4L)",
+      "rating": "465",
+      "reviews": "636",
+      "image": "https://m.media-amazon.com/images/I/51cU7HevYPL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Multilaser G Max 2 4G 64GB Tela HD+ 6.5 pol. Dual Chip 2GB RAM Câmera Tripla 8MP + 5MP Preto - P9168",
+      "rating": "435",
+      "reviews": "78",
+      "image": "https://m.media-amazon.com/images/I/51dHIniwLOL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi POCO X6 5G 8GB+256GB Global Version NFC Snapdragon 7s Gen 2 Smartphone 120Hz FIow AMOLED 64MP Triple Camera With OIS (Black)",
+      "rating": "475",
+      "reviews": "36",
+      "image": "https://m.media-amazon.com/images/I/71FJJongMOL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Celular Xiaomi Redmi 12C 128GB / 4GB RAM/Dual Sim/Tela 6.71\" / Câmeras 50MP+0.08MP e 5MP - Graphite Gray (Global)",
+      "rating": "465",
+      "reviews": "2981",
+      "image": "https://m.media-amazon.com/images/I/71izFpyABiL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone realme C55 Memória 8+256GB Câmera 64MP Frontal 8MP Tela 6.72\" Carregador 33W Dual Chip",
+      "rating": "475",
+      "reviews": "394",
+      "image": "https://m.media-amazon.com/images/I/61plzuuI6cL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Samsung Galaxy A04e 64GB 4G Wi-Fi Tela 6.5'' Dual Chip 3GB RAM Câmera Dupla + Selfie 5MP com modo Retrato Bateria de 5000mAh - Preto",
+      "rating": "465",
+      "reviews": "878",
+      "image": "https://m.media-amazon.com/images/I/818E5eDxVKL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Motorola Moto E22 4G 128GB 4GB RAM Azul",
+      "rating": "465",
+      "reviews": "2349",
+      "image": "https://m.media-amazon.com/images/I/514rShwNkWL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Poco X5 5g Dual Sim 256gb 8gb Wildcat Blue India",
+      "rating": "445",
+      "reviews": "197",
+      "image": "https://m.media-amazon.com/images/I/61Nc5kl44jL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Redmi Note 13 6GB+128GB Global Version Powerful Snapdragon® performance 120Hz FHD+ AMOLED display 33W fast charging with 5000mAh battery (Black)",
+      "rating": "495",
+      "reviews": "40",
+      "image": "https://m.media-amazon.com/images/I/51pk9ttmZqL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Xiaomi POCOPHONE X5 5G (8+256GB, Preto)",
+      "rating": "475",
+      "reviews": "7143",
+      "image": "https://m.media-amazon.com/images/I/51tqdlQSgqL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi POCO X6 Pro 5G 12GB+512GB NFC Dimensity 8300-Ultra 64MP triple camera 67W 120Hz AMOLED Global Version (Cinza)",
+      "rating": "455",
+      "reviews": "52",
+      "image": "https://m.media-amazon.com/images/I/516kEGbFG1L._AC_UL320_.jpg"
+    },
+    {
+      "title": "Samsung Galaxy A03 Core 32GB 4G Wi-Fi Tela 6.5'' Dual Chip 2GB RAM Câmera 8MP + Selfie 5MP Bateria de 5000mAh - Verde",
+      "rating": "445",
+      "reviews": "101",
+      "image": "https://m.media-amazon.com/images/I/71UTZh9brgL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Motorola Moto G73 5G 128GB - Branco, RAM 8GB, Câmera 50MP, Selfie 16MP e Tela 6,5\"",
+      "rating": "465",
+      "reviews": "122",
+      "image": "https://m.media-amazon.com/images/I/51pM+FLjmbL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Realme C53 RMX 3760 4G LTE, RAM 8GB + 256GB, Champion Gold - Dourado Versão Global",
+      "rating": "425",
+      "reviews": "13",
+      "image": "https://m.media-amazon.com/images/I/619QlGnpsVL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Celular Positivo P28, Tela 2.8'', 24MB RAM e 32MB Armazenamento, 2G, Bluetooth, Dual SIM - Preto",
+      "rating": "445",
+      "reviews": "1006",
+      "image": "https://m.media-amazon.com/images/I/512Ne+zYFAL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Note 12 5G 256GB - 8GB Ram (Versao Global) (Onyx Gray)",
+      "rating": "475",
+      "reviews": "866",
+      "image": "https://m.media-amazon.com/images/I/81CXAKm8F0L._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi POCO X6 5G 12GB+256GB Global Version NFC Snapdragon 7s Gen 2 Smartphone 120Hz FIow AMOLED 64MP Triple Camera With OIS (Blue)",
+      "rating": "485",
+      "reviews": "29",
+      "image": "https://m.media-amazon.com/images/I/81h7PrHrI1L._AC_UL320_.jpg"
+    },
+    {
+      "title": "Samsung Galaxy A03 Core 32GB 4G Wi-Fi Tela 6.5'' Dual Chip 2GB RAM Câmera 8MP + Selfie 5MP Bateria de 5000mAh - Preto",
+      "rating": "465",
+      "reviews": "202",
+      "image": "https://m.media-amazon.com/images/I/81B+Rti5d+L._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Nokia C12 4G 64 GB + Até 4 GB RAM* Tela 6,3\" HD+ Câmera com IA Android Desbloqueio Facial + Capa/Película/Fone/Carregador - Cinza - NK120",
+      "rating": "365",
+      "reviews": "14",
+      "image": "https://m.media-amazon.com/images/I/61CdqAKjvUL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Xiaomi Redmi Note 12 128GB - 4GB Ram - Versao Global (Onyx Gray)",
+      "rating": "475",
+      "reviews": "7124",
+      "image": "https://m.media-amazon.com/images/I/51fAmmSJcUL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Samsung Galaxy A14 4G , Prata , 128GB",
+      "rating": "475",
+      "reviews": "733",
+      "image": "https://m.media-amazon.com/images/I/81G60ui-JKL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Smartphone Samsung Galaxy A54 5G 128GB 8GB RAM Tela 6.4'' Câmera Tripla de até 50MP + Selfie 32MP - Branco",
+      "rating": "485",
+      "reviews": "454",
+      "image": "https://m.media-amazon.com/images/I/61IgJvzu1eL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Xiaomi POCO X5 5G Smartphone Dual Sim 256GB Memory 8GB RAM 6.67\" AMOLED Display 5000mAh 48MP+13MP CAM (verde)",
+      "rating": "475",
+      "reviews": "963",
+      "image": "https://m.media-amazon.com/images/I/51qXb8txLIL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Apple iPhone 13 Pro (256 GB) - Verde-alpino",
+      "rating": "495",
+      "reviews": "1389",
+      "image": "https://m.media-amazon.com/images/I/411wY2hpzuL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Apple iPhone 14 Pro Max (128 GB) – Dourado",
+      "rating": "485",
+      "reviews": "1009",
+      "image": "https://m.media-amazon.com/images/I/51GZNF-UxFL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Apple iPhone 11 (64 GB) Preto",
+      "rating": "485",
+      "reviews": "3357",
+      "image": "https://m.media-amazon.com/images/I/71iO2R+CLjL._AC_UL320_.jpg"
+    },
+    {
+      "title": "Apple iPhone 14 Pro (512 GB) – Dourado",
+      "rating": "485",
+      "reviews": "623",
+      "image": "https://m.media-amazon.com/images/I/51PNqMth7kL._AC_UL320_.jpg"
+    }
+  ]
+}
